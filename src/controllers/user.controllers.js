be8de53e1faf0cae68f9dcb9174839e9ddd1d44e
Ishika Goyal <ishikagoyal336@gmail.com>
2025-09-12 17:26:49 +0530

    fixed refreshtoken

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -172,7 +172,7 @@ const refreshAccessToken = asyncHandler(async(req,res)=>
 {
    const incomingAccessToken = req.cookies.refreshToken || req.body.refreshToken
 
-   if(incomingRefreshToken){
+   if(!incomingRefreshToken){
       throw new ApiError(401,"Unauthorized request")
    }
 
